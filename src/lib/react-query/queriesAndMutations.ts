cb91fbd2cf6d8b046903b1f20ebf7289eaa1e081
Fudi <fudi.mahfudin@gmail.com>
2023-12-07 20:21:01 +0700

    Create Home navigation

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,6 +1,6 @@
 import { INewUser } from "@/types";
 import { useMutation, } from "@tanstack/react-query";
-import { createUserAccount, signInAccount } from "../appwrite/api";
+import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
 
 
 export const useCreateUserAccount = () => {
@@ -16,4 +16,10 @@ export const useSignInAccount = () => {
       password: string;
     }) => signInAccount(user),
   });
+}
+
+export const useSignOutAccount = () => {
+  return useMutation({
+    mutationFn: signOutAccount
+  })
 }
